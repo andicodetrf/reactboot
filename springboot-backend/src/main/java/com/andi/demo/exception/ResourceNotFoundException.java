6a3c9fb53639131794fc38216271615dd82d5d99
andicodetrf <andrea_lau92@hotmail.com>
2022-02-26 12:42:58 +0800

    add comments on sb exception

--- springboot-backend/src/main/java/com/andi/demo/exception/ResourceNotFoundException.java
+++ springboot-backend/src/main/java/com/andi/demo/exception/ResourceNotFoundException.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 //custom exception - when the record doesnt exist in db, api will return http status not found to client
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+	
+	//it is good practice to provide the serialVersionUID value and update it after changes to the class 
+	//so that we can have control over the serialization/deserialization process and to avoid unwanted incompatibility 
 	private static final long serialVersionUID = 1L;
 	
 	public ResourceNotFoundException(String message) {
